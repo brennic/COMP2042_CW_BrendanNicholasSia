c8ce12397cf7510ff93e71c103d97ddf4e746ce4
brennic <73512004+brennic@users.noreply.github.com>
2020-12-12 16:26:28 +0800

    change new level

--- Level2.java
+++ Level2.java
@@ -3,166 +3,63 @@ package p4_group_8_repo;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
 
 import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
-//MainMenu
-import javafx.application.Application;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-	AnimationTimer timer;
-	MyStage background;
+public class Level2 {
+
+	MyStage background2;
 	Animal animal;
-	Number num;
+	AnimationTimer timer;
 	
-	public void start(Stage primaryStage) throws Exception {
+	public void lvl2(Stage primaryStage2) throws Exception{
 		
-		background = new MyStage();
-		Scene scene  = new Scene(background,600,800);//Frogger Game
+		background2 = new MyStage();
+		Scene scene2  = new Scene(background2,600,800);//Frogger Game
 		
-		  //Menu background image
-		  Image image1 = new Image("file:src/p4_group_8_repo/mainmenubg.png", 600, 800, true, true);
-		  //Creating the image view
-	      ImageView imageView1 = new ImageView();
-	    //Setting image to the image view
-	      imageView1.setImage(image1);
-	    //Setting the image view parameters
-	      imageView1.setX(10);
-	      imageView1.setY(10);
-	      imageView1.setFitWidth(600);
-	      imageView1.setFitHeight(800);
-	      imageView1.setPreserveRatio(true);
-	      //Setting the Scene object
-	      Group root1 = new Group(imageView1);
-		  
-		  //Manual Image
-	      Image image = new Image("file:src/p4_group_8_repo/manual.png", 600, 800, true, true);
-	      //Creating the image view
-	      ImageView imageView = new ImageView();
-	      //Setting image to the image view
-	      imageView.setImage(image);
-	      //Setting the image view parameters
-	      imageView.setX(10);
-	      imageView.setY(10);
-	      imageView.setFitWidth(575);
-	      imageView.setPreserveRatio(true);
-	      //Setting the Scene object
-	      Group root = new Group(imageView);
-	      
-	      Scene manualscene = new Scene(root);
-	   	  
-	      
-	  //Main Menu 
-			Pane mainMenuRoot = new Pane();			
-			
-			//River level button
-	  		Button btnstart = new Button("River Level");
-	  		btnstart.setTranslateX(150);
-	  	    btnstart.setTranslateY(-77);
-	  	    btnstart.setOnMouseClicked(event -> { 
-	  			primaryStage.setScene(scene);
-	  			background.start();
-	  			start();
-	  	    });
-	  	    
-	  	//Sky level button
-	  		Button btnstart2 = new Button("Sky Level");
-	  		btnstart2.setTranslateX(270);
-	  	    btnstart2.setTranslateY(-123);
-	  	    btnstart2.setOnMouseClicked(event -> { 
-	  			primaryStage.setScene(scene);
-	  			background.start();
-	  			start();
-	  	    });
-	  	    
-	  //Manual Button
-	  	    Button btnmanual = new Button("How to Play?");
-	  	    btnmanual.setTranslateX(5);
-	  	    btnmanual.setTranslateY(-147);
-	  	    btnmanual.setOnMouseClicked(event ->{
-	  	    	
-	  	    	primaryStage.setScene(manualscene);
-	  	    	primaryStage.show();
-	  	    });
-	  	    
-	  	    
-	  //Exit Button
-	  	  Button btnexit = new Button("Exit!");
-	  	  btnexit.setTranslateX(555);
-	  	  btnexit.setTranslateY(-193);
-	  	  btnexit.setOnAction(event -> System.exit(0));
-	  	  
-	  	  VBox layout1 = new VBox(20); 
-	  	  layout1.getChildren().addAll(root1, btnstart, btnstart2,  btnmanual, btnexit);
-	  	  Scene menuscene = new Scene(layout1,600,330); // Game Menu
-	  	  
-		//Obstacle obstacle = new Obstacle("file:src/p4_group_8_repo/truck1Right.png", 25, 25, 3);
-		//Obstacle obstacle1 = new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 100, 100,2 );
-		//Obstacle obstacle2 = new Obstacle("file:src/p4_group_8_repo/truck1Right.png",0,  150, 1);
-	  	  
-		BackgroundImage froggerback = new BackgroundImage("file:src/p4_group_8_repo/arcade1.png");
+		primaryStage2.setScene(scene2);
+		background2.start();
+		start();
+		
+		
+		
+		BackgroundImage froggerback2 = new BackgroundImage("file:src/p4_group_8_repo/Lvl2.png");
 	    
 		
-		background.add(froggerback);
+		background2.add(froggerback2);
 		
 		
 		
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 0, 166, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 220, 166, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 440, 166, 0.75));
+		background2.add(new Log("file:src/p4_group_8_repo/Bird2-right.PNG", 150, 0, 166, 0.75));
+		background2.add(new Log("file:src/p4_group_8_repo/Bird2-right.PNG", 150, 220, 166, 0.75));
+		background2.add(new Log("file:src/p4_group_8_repo/Bird2-right.PNG", 150, 440, 166, 0.75));
 		//background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 0, 166, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/logs.png", 300, 0, 276, -2));
-		background.add(new Log("file:src/p4_group_8_repo/logs.png", 300, 400, 276, -2));
+		background2.add(new Log("file:src/p4_group_8_repo/Bird1- right.png", 300, 0, 276, -2));
+		background2.add(new Log("file:src/p4_group_8_repo/Bird1- right.png", 300, 400, 276, -2));
 		//background.add(new Log("file:src/p4_group_8_repo/logs.png", 300, 800, 276, -2));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 50, 329, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 270, 329, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 490, 329, 0.75));
+		background2.add(new Log("file:src/p4_group_8_repo/Birds2-right.PNG", 150, 50, 329, 0.75));
+		background2.add(new Log("file:src/p4_group_8_repo/Birds2-right.PNG", 150, 270, 329, 0.75));
+		background2.add(new Log("file:src/p4_group_8_repo/Birds2-right.PNG", 150, 490, 329, 0.75));
 		//background.add(new Log("file:src/p4_group_8_repo/log3.png", 150, 570, 329, 0.75));
 		
-		background.add(new Turtle(500, 376, -1, 130, 130));
-		background.add(new Turtle(300, 376, -1, 130, 130));
-		background.add(new WetTurtle(700, 376, -1, 130, 130));
-		background.add(new WetTurtle(600, 217, -1, 130, 130));
-		background.add(new WetTurtle(400, 217, -1, 130, 130));
-		background.add(new WetTurtle(200, 217, -1, 130, 130));
+		/*
+		background2.add(new Turtle(500, 376, -1, 130, 130));
+		background2.add(new Turtle(300, 376, -1, 130, 130));
+		background2.add(new WetTurtle(700, 376, -1, 130, 130));
+		background2.add(new WetTurtle(600, 217, -1, 130, 130));
+		background2.add(new WetTurtle(400, 217, -1, 130, 130));
+		background2.add(new WetTurtle(200, 217, -1, 130, 130));
+		*/
+		
 		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 200, 100, 1));
 		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 0, 100, 1));
 		//background.add(new Log("file:src/p4_group_8_repo/log2.png", 100, 120, -1));
@@ -182,40 +79,31 @@ public class Main extends Application {
 		//End end3 = new End();
 		//End end4 = new End();
 		//End end5 = new End();
-		background.add(new End(13,96));
-		background.add(new End(141,96));
-		background.add(new End(141 + 141-13,96));
-		background.add(new End(141 + 141-13+141-13+1,96));
-		background.add(new End(141 + 141-13+141-13+141-13+3,96));
+		background2.add(new End(13,96));
+		background2.add(new End(141,96));
+		background2.add(new End(141 + 141-13,96));
+		background2.add(new End(141 + 141-13+141-13+1,96));
+		background2.add(new End(141 + 141-13+141-13+141-13+3,96));
 		animal = new Animal("file:src/p4_group_8_repo/froggerUp.png");
-		background.add(animal);
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 0, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 300, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 600, 649, 1, 120, 120));
+		background2.add(animal);
+		background2.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 0, 649, 1, 120, 120));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 300, 649, 1, 120, 120));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 600, 649, 1, 120, 120));
 		//background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 720, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 100, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 250, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 400, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 550, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 0, 540, 1, 200, 200));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 500, 540, 1, 200, 200));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 500, 490, -5, 50, 50));
-		background.add(new Digit(0, 30, 360, 25));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 100, 597, -1, 50, 50));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 250, 597, -1, 50, 50));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 400, 597, -1, 50, 50));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 550, 597, -1, 50, 50));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 0, 540, 1, 200, 200));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 500, 540, 1, 200, 200));
+		background2.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 500, 490, -5, 50, 50));
+		background2.add(new Digit(0, 30, 360, 25));
 		
 		
 		//background.add(obstacle);
 		//background.add(obstacle1);
 		//background.add(obstacle2);
-		
-		primaryStage.setScene(menuscene);
-		primaryStage.show();
-		//start();
 	}
-	
-	/*catch (Exception e) { 
-		  
-        System.out.println(e.getMessage()); 
-    } */
 	
 	public void createTimer() {
 		timer = new AnimationTimer() {
@@ -236,9 +124,9 @@ public class Main extends Application {
 	        	
 	        	if (animal.getStop()) {
 	        		System.out.print("STOPP:");
-	        		background.stopMusic();
+	        		background2.stopMusic();
 	        		stop();
-	        		background.stop();
+	        		background2.stop();
 	        		}
 	        		/*
 	        		Alert alert = new Alert(AlertType.INFORMATION);
@@ -372,10 +260,8 @@ public class Main extends Application {
 	                String currentLine = reader1.readLine();          
 	                while (currentLine != null)
 	                {
-	                	for(int i = 0; i < 5; i++) {
 	                    lines1.add(currentLine);
 	                    currentLine = reader1.readLine();
-	                	}
 	                }
 	        	Alert alert = new Alert(AlertType.INFORMATION);
 	    		alert.setTitle("You Have Won The Game!");
@@ -394,34 +280,26 @@ public class Main extends Application {
 		
     }
 	
-	//public void start:
 	public void start() {
-		background.playMusic();
+		background2.playMusic();
     	createTimer();
         timer.start();
     }
-
 	
-	
-    public void stop(Stop gameover) {
-        //timer.stop();
-    	gameover.stopgame();
-    }
-    
-	
-    public void setNumber(int i) {
-    	int shift = 0;
-    	while (i > 0) {
-    		  int d = i / 10;
-    		  int k = i - d * 10;
-    		  i = d;
-    		  background.add(new Digit(k, 30, 360 - shift, 25));
-    		  shift+=30;
-    		}
-    }
-    
-    public static void main(String[] args) {
-		launch(args);
-	}
-    
+	 public void stop(Stop gameover) {
+	        //timer.stop();
+	    	gameover.stopgame();
+	    }
+	    
+		
+	    public void setNumber(int i) {
+	    	int shift = 0;
+	    	while (i > 0) {
+	    		  int d = i / 10;
+	    		  int k = i - d * 10;
+	    		  i = d;
+	    		  background2.add(new Digit(k, 30, 360 - shift, 25));
+	    		  shift+=30;
+	    		}
+	    }
 }
